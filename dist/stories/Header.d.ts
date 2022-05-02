40d270e488bf3d48cb4f29d6ade3ab82dc063298
Joel <teslavargas18@gmail.com>
2022-05-02 17:26:43 -0300

    feat: se agregó semantic-versioning

---
+++ dist/stories/Header.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+import "./header.css";
+export interface HeaderProps {
+    user?: {};
+    onLogin: () => void;
+    onLogout: () => void;
+    onCreateAccount: () => void;
+}
+export declare const Header: ({ user, onLogin, onLogout, onCreateAccount, }: HeaderProps) => JSX.Element;
+export default Header;
